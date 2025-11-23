14312a7c058fcd25c944336af47ec7a8f62dffb8
Steban Cantor <bscantor@gmail.com>
2025-11-22 20:09:49 -0500

    fixes

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -4,4 +4,6 @@ import io from "socket.io-client";
 const DOMAIN = import.meta.env.DOMAIN || 'http://localhost';
 const SERVER_PORT = import.meta.env.SERVER_PORT || '3001';
 
+console.log(`Connecting from Client to ${DOMAIN}:${SERVER_PORT}`)
+
 export const socket = io(`${DOMAIN}:${SERVER_PORT}`);
